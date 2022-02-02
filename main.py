014aec68598378488ec695035d06244fb5bd7a81
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-02 06:49:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors"))
+
+possibilities = [rock, paper, scissors]
+print(f"You choose\n{possibilities[choice]}")
+computer_choice = random.randint(0, 2)
+
